b82e2aa1144ac16ecdaa48d470c321927b4cd9f7
Travis Young <t.w.young.ubc@gmail.com>
2018-04-06 16:05:23 -0400

    fixed moment deprecation warning

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -4,7 +4,7 @@ import './Event.css';
 
 const Event = ({ event, backgroundColor}) => {
   const { hour, minute, day, month, year, name,  } = event;
-  const date = moment(`${year}-${month}-${day}`).format('D MMMM YYYY');
+  const date = moment(`${year}-${month}-${day}`, 'YYYY-MM-DD').format('D MMMM YYYY');
   return (
     <div
       className="event"
