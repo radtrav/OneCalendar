441aea129711c5f7769280b12ff1af55278de72d
Travis Young <t.w.young.ubc@gmail.com>
2018-04-06 15:30:35 -0400

    made mobile responsive

--- src/components/Month/Month.js
+++ src/components/Month/Month.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import MonthHeader from '../MonthHeader/MonthHeader';
 import Weeks from '../Weeks/Weeks';
+import './Month.css';
 
 const Month = ({ referenceDate, events }) => {
   return (
-    <div>
+    <div className="month-wrapper">
       <MonthHeader />
       <Weeks referenceDate={referenceDate} />
     </div>
--- src/components/MonthHeader/MonthHeader.js
+++ src/components/MonthHeader/MonthHeader.js
@@ -5,7 +5,7 @@ const WEEK_DAYS = ['Su', 'Mo', 'Tu', 'We', "Th",'Fr','Sa'];
 
 const MonthHeader = () => (
   <div className="month-header-wrapper">
-    {WEEK_DAYS.map(day => <div className="month-header"> {day} </div>)}
+    {WEEK_DAYS.map(day => <div key={day} className="month-header"> {day} </div>)}
   </div>
 );
 
