4e851e6a301c2b2069353341ef543b6a2c70c160
Travis Young <t.w.young.ubc@gmail.com>
2018-04-06 16:50:02 -0400

    changed the default events

--- src/reducers/eventsReducer.js
+++ src/reducers/eventsReducer.js
@@ -6,7 +6,6 @@ const DEFAULT_EVENTS = [
   { hour: 9, minute: 30, day: 5, month: 3, year: 2018, name: 'Eat Pie' },
   { hour: 6, minute: 30, day: 14, month: 4, year: 2018, name: 'Watch Die Hard' },
   { hour: 1, minute: 25, day: 1, month: 4, year: 2018, name: 'Fly a Kite' },
-  { hour: 8, minute: 20, day: 17, month: 4, year: 2018, name: 'Watch Die Hard Again' },
   { hour: 10, minute: 20, day: 15, month: 4, year: 2018, name: 'Prep for Lunch' },
   { hour: 10, minute: 30, day: 22, month: 5, year: 2018, name: 'Make Pasta' },
   { hour: 12, minute: 30, day: 22, month: 5, year: 2018, name: 'Walk Dog' },
@@ -21,6 +20,5 @@ export default function events(state = DEFAULT_EVENTS, action) {
   }
 }
 
-// TODO sort by time and not just date
 export const getEventsByMonth = (events, month) =>
   events.filter(event => event.month - 1 === month).sort((a, b) => a.day - b.day);
